2f05dbb937860dbcc107cfa4d7682a19b83ee28b
Vinicius <vini169ofi@gmail.com>
2025-12-06 22:31:05 -0300

    Consulta Cliente (sem pesquisa)

---
+++ ProjetoFinal/Controllers/ConsultaClienteController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoFinal.Repositorio;
+
+namespace ProjetoFinal.Controllers
+{
+    public class ConsultaClienteController : Controller
+    {
+        private readonly CadastroClienteRepositorio _repo;
+
+        public ConsultaClienteController(CadastroClienteRepositorio repo)
+        {
+            _repo = repo;
+        }
+
+        public IActionResult ConsultaCliente()
+        {
+            var clientes = _repo.ListarClientes();
+            return View(clientes);
+        }
+    }
+}
